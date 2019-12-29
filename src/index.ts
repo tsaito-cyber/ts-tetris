83e05cdb8cdc68b8df9f701a41f8a23b4442e05c
takuma.saito <takuma.saito@theport.jp>
2019-12-29 18:48:58 +0900

    block generator を追加

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,14 @@ enum BlockState {
     CentralUnboundedBlock = 'X'
 }
 
+enum BoardState {
+    Moving,
+    Creating,
+    Stopping,
+    Eliminating,
+    Ending,
+}
+
 const boardRaw = `
 *..........*
 *..........*
@@ -31,18 +39,45 @@ const boardRaw = `
 ************
 `.trim()
 
-class Board {
-    private board: Array<Array<BlockState>>
-    constructor(boardRaw: string) {
-        this.board = boardRaw.split("\n").map(row => {
-            return [...row].map(char => char as BlockState)
-        })
+type BlockStates = Array<Array<BlockState>>
+
+function* blockGen() {
+    function randBlocks(): Board {
+        return Blocks[Math.floor(Math.random() * Blocks.length)]
     }
-    public to_s = (): string => {
+    let items = []
+    for (let i = 0; i < 3; i++) {
+        items.push(randBlocks())
+    }
+    while(true) {
+        yield items
+        items.shift() // discard
+        items.push(randBlocks())
+    }
+}
+
+class Board {
+    private board: BlockStates
+    constructor(str: string) {
+        this.board = str.split("\n").map(row => [...row].map(char => char as BlockState))
+    }
+    get boardText() {
         return this.board.map(row => row.map(r => r as string).join('')).join("\n")
     }
 }
 
+const Blocks = [
+    "...\n.X.\n111", // T
+    "...\n1X.\n.11", // Z
+    "...\n.X1\n11.", // Z_t
+    ".1.\n.X.\n.11", // L
+    ".1.\n.X.\n11.", // L_t
+].map(str => new Board(str))
+
 let board = new Board(boardRaw)
-console.log(board.to_s())
+let gen = blockGen();
+for (let i = 0; i < 3; i++) {
+    let result = gen.next().value as Array<Board>
+    console.log(result[0].boardText)
+}
 
