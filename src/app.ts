ccf83e5037085f827f3cb5085aa11348162920f1
takuma.saito <takuma.saito@theport.jp>
2019-12-30 16:08:08 +0900

    can move の微妙な不具合修正した

--- src/app.ts
+++ src/app.ts
@@ -105,8 +105,8 @@ class Board {
     canMoveBottom(other: BoardLayer): boolean {
         if (!this.isPuttable(other)) { return false }
         return Array.from(other.iterator())
-            .every(({y: y, x: x, value: value}) => {
-                value === PointState.Empty || (this.table[y+1][x] === PointState.Empty) })
+            .every(({y: y, x: x, value: value}) =>
+                   value === PointState.Empty || (this.table[y+1][x] === PointState.Empty))
     }
     static fromString(str: string): Board {
         return new this(strToTable(str))
@@ -158,7 +158,7 @@ class BoardLayer extends Board {
 
 const board = Board.fromString(boardRaw)
 const gen = blockGen();
-for (let i = 0; i < 12; i++) {
+for (let i = 0; i < 20; i++) {
     const b = gen.next().value as BoardLayer
     if (!board.isPuttable(b)) { continue }
     console.log(`move ok: ${board.canMoveBottom(b)}`)
